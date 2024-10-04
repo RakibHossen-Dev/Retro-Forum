1f1fe5e2d93553224ee41047319aa7b055808143
Rakib <hellorakibhossen@gmail.com>
2024-10-04 02:09:55 -0700

    full website done

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ const loadAllPosts = async (category) => {
   //   } else {
   //     console.log(`https://openapi.programming-hero.com/postsapi/retro-forum/`);
   //   }
+
+  document.getElementById("post-container").innerHTML = "";
   const response = await fetch(
     `https://openapi.programming-hero.com/api/retro-forum/posts${
       category ? `?category=${category}` : ""
@@ -117,4 +119,64 @@ const handleSearchByCategory = () => {
   const serchText = document.getElementById("searchPosts").value;
 
   loadAllPosts(serchText);
+};
+
+const latestPosts = async () => {
+  const response = await fetch(
+    "https://openapi.programming-hero.com/api/retro-forum/latest-posts"
+  );
+  const data = await response.json();
+  dusplyPosts(data);
+};
+latestPosts();
+
+const dusplyPosts = (p) => {
+  const latestPostContainer = document.getElementById("latest-post-container");
+
+  p.forEach((e) => {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+<div class="card lg:w-96 lg:h-[550px] h-auto pb-5 bg-base-100 shadow-2xl">
+            <figure class="lg:px-6 px-4 pt-4 lg:pt-8">
+              <img src="${e.cover_image}" alt="Shoes" class="rounded-xl" />
+            </figure>
+            <div class="p-5 lg:p-10 space-y-4 lg:space-y-5">
+              <p class="opacity-50 text-start">
+                <i class="fa-solid fa-calendar-days me-2"></i
+                >${
+                  e.author.posted_date
+                    ? e.author.posted_date
+                    : "No publish date"
+                }
+              </p>
+              <h2 class="card-title text-start">${e.title}</h2>
+              <p class="text-start">${e.description}</p>
+              <div class="card-actions flex gap-5 items-center">
+                <div class="avatar">
+                  <div
+                    class="lg:w-12 w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2"
+                  >
+                    <img src="${e.profile_image}" />
+                  </div>
+                </div>
+                <div>
+                  <h3 class="text-start font-extrabold">${e.author.name}</h3>
+                  <p class="text-start opacity-60">
+                    ${e.author.designation ? e.author.designation : "Unknown"}
+                  </p>
+                </div>
+              </div>
+
+              <span
+                id="latestPostLoader"
+                class="loading loading-infinity loading-lg lg:mt-24 text-primary hidden"
+              >
+              </span>
+              <!-- dynamic content -->
+            </div>
+          </div>
+    `;
+    latestPostContainer.appendChild(div);
+  });
 };
